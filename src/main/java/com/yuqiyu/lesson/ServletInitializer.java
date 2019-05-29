a222c1bb654a3a4d04e027af2987cc713e264ed7
liusha <1253891026@qq.com>
2019-05-29 09:48:11 +0800

    HelloWord

---
+++ src/main/java/com/yuqiyu/lesson/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.yuqiyu.lesson;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(LessonApplication.class);
+    }
+
+}
---
+++ src/main/java/com/yuqiyu/lesson/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.yuqiyu.lesson.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class HelloWorldController {
+@RequestMapping(value = "/index",method = RequestMethod.GET)
+    public String index(){
+        return "HelloWord";
+    }
+
+}
